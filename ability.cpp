aeba8a6605e1743434ed55d03fa8dbe36e7db239
Bruno <bruno.exler@gmail.com>
2026-03-25 14:53:47 +0100

    cleanup

--- ability.cpp
+++
@@ -1,9 +0,0 @@
-#include "ability.h"
-#include <iostream>
-
-Ability::Ability(std::string name, int damage) : name(name), damage(damage) {}
-
-std::string Ability::get_name() {
-    std::cout << this->name << std::endl;
-    return name;
-}
